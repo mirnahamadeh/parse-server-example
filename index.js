a2367eece996ea7cb42d8db6793cb633e0528ec3
mirnahamadeh <gardenia_0809@hotmail.com>
2018-11-08 20:00:28 +0200

    Email

--- index.js
+++ index.js
@@ -17,9 +17,18 @@ var api = new ParseServer({
   appId: process.env.APP_ID || 'myAppId',
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
-  liveQuery: {
-    classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
-  }
+  emailAdapter: {
+    module: 'parse-server-simple-mailgun-adapter',
+    options: {
+      // The address that your emails come from
+      fromAddress: 'gardenia_0809@hotmail.com',
+      // Your domain from mailgun.com
+      domain: 'sandboxde4f96b4c33f44588c02d62195ddb7cd.mailgun.org',
+      // Your API key from mailgun.com
+      apiKey: 'api:5dd330e7bc8a7241357be5854dc98889-4412457b-3e138ef5',
+    }
+  },
+    verifyUserEmails: true
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
